edc170abc414b0537babfc4a1c205fb8af94ce67
monicaf77 <80273396+monicaf77@users.noreply.github.com>
2023-09-27 13:16:45 +0100

    Update fp_adder_test.vhd

--- lab2_sources/fp_adder_test.vhd
+++ lab2_sources/fp_adder_test.vhd
@@ -4,11 +4,9 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 entity fp_adder_test is
    port(
-      clk: in std_logic;
-      sw: in std_logic_vector(7 downto 0);
-      btn: in std_logic_vector(3 downto 0);
-      an: out std_logic_vector(3 downto 0);
-      sseg: out std_logic_vector(7 downto 0)
+      sw: in std_logic_vector(15 downto 0);
+      btn: in std_logic_vector(1 downto 0);
+      led: out std_logic_vector(15 downto 0)
    );
 end fp_adder_test;
 
@@ -19,16 +17,15 @@ architecture arch of fp_adder_test is
    signal sign_out: std_logic;
    signal exp_out: std_logic_vector(3 downto 0);
    signal frac_out: std_logic_vector(7 downto 0);
-   signal led3, led2, led1, led0:
-             std_logic_vector(7 downto 0);
+  
 begin
    -- set up the fp adder input signals
-   sign1 <= '0';
-   exp1 <= "1000";
-   frac1<= '1' &  sw(1) & sw(0) & "10101";
-   sign2 <= sw(7);
-   exp2 <= btn;
-   frac2 <= '1' & sw(6 downto 0);
+   sign1 <= btn(0);
+   exp1 <= sw(15 downto 12);
+   frac1<= sw(11 downto 8) & "0000";
+   sign2 <= btn(1);
+   exp2 <= sw(7 downto 4);
+   frac2 <= sw(3 downto 0) & "0000";
 
    -- instantiate fp adder
    fp_add_unit: entity work.fp_adder
@@ -39,27 +36,6 @@ begin
          frac_out=>frac_out
       );
 
-   -- instantiate three instances of hex decoders
-   -- exponent
-   sseg_unit_0: entity work.hex_to_sseg
-      port map(hex=>exp_out, dp=>'0', sseg=>led0);
-   -- 4 LSBs of fraction
-   sseg_unit_1: entity work.hex_to_sseg
-      port map(hex=>frac_out(3 downto 0),
-               dp=>'1', sseg=>led1);
-   -- 4 MSBs of fraction
-   sseg_unit_2: entity work.hex_to_sseg
-      port map(hex=>frac_out(7 downto 4),
-               dp=>'0', sseg=>led2);
-   -- sign
-   led3 <= "11111110" when sign_out='1' else -- middle bar
-           "11111111";                       -- blank
-
-   -- instantiate 7-seg LED display time-multiplexing module
-   disp_unit: entity work.disp_mux
-      port map(
-         clk=>clk, reset=>'0',
-         in0=>led0, in1=>led1, in2=>led2, in3=>led3,
-         an=>an, sseg=>sseg
-      );
+    led<= sign_out & "000" & exp_out & frac_out;
+   
 end arch;
