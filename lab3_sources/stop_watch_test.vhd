09813f0235c9fdc1e023a0b99da0bf158d45d7a5
monicaf77 <80273396+monicaf77@users.noreply.github.com>
2023-10-03 15:28:10 +0100

    Add files via upload

---
+++ lab3_sources/stop_watch_test.vhd
@@ -0,0 +1,29 @@
+--Listing 4.19
+library ieee;
+use ieee.std_logic_1164.all;
+entity stop_watch_test is
+   port(
+      clk: in std_logic;
+	  rst: in std_logic;
+      btn: in std_logic_vector(1 downto 0);
+      an: out std_logic_vector(3 downto 0);
+      sseg: out std_logic_vector(7 downto 0)
+   );
+end stop_watch_test;
+
+architecture arch of stop_watch_test is
+   signal d2, d1, d0: std_logic_vector(3 downto 0);
+begin
+   disp_unit: entity work.disp_hex_mux
+      generic map (N => 18)
+	  port map(
+         clk=>clk, reset=>rst,
+         hex3=>"0000", hex2=>d2,
+         hex1=>d1, hex0=>d0,
+         dp_in=>"1101", an=>an, sseg=>sseg);
+
+  watch_unit: entity work.stop_watch(cascade_arch)
+     port map(
+        clk=>clk, rst=>rst, go=>btn(1), clr=>btn(0),
+        d2 =>d2, d1=>d1, d0=>d0 );
+end arch;
